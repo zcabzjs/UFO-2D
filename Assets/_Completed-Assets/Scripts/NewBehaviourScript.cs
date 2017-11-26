d0a47e5d1cfdc7dcb69ae1baf4a9e12237bd140f
Zi Jian Sim <zijian12345@hotmail.com>
2017-11-26 21:22:48 +0000

    Transferring to Github

---
+++ Assets/_Completed-Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+	}
+}
---
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private int count;
+	public Text scoreText;
+	public Text victoryText;
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		count = 0;
+		ScoreText ();
+		victoryText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.CompareTag("Pick Up")){
+			other.gameObject.SetActive (false);
+			count++;
+			ScoreText ();
+		}
+		if (count >= 12) {
+			victoryText.text = "VI VON!!!!!";
+		}
+	}
+
+	void ScoreText(){
+		scoreText.text = "Score:" + count.ToString ();
+	}
+		
+}
